ea2904e48982868c9fca010b6cc5dc27e99d3a1e
matrag71 <heyaanurag@gmail.com>
2024-05-13 21:16:09 +0530

    ASDAS

--- app/(root)/(home)/layout.tsx
+++ app/(root)/(home)/layout.tsx
@@ -5,7 +5,7 @@ import Navbar from '@/components/Navbar';
 import Sidebar from '@/components/Sidebar';
 
 export const metadata: Metadata = {
-  title: 'MANSM PROJECT 2',
+  title: 'DEV-ANU',
   description: 'A workspace for your team, powered by Stream Chat and Clerk.',
 };
 
